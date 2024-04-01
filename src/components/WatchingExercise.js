b656ac4b60321334d93aa78437edb65690d1f00d
Mohameed7993 <hmody.chechareto@gmail.com>
2024-04-01 06:34:24 +0300

    It's ready to deployment frontend

--- src/components/WatchingExercise.js
+++ src/components/WatchingExercise.js
@@ -44,26 +44,24 @@ const ExerciseWatching = ({ isOpen, VideoID, onClose, videoName, videoHowtodo })
   if (!isOpen || !VideoID || !videoUrl) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="relative bg-white p-6 rounded-lg shadow-lg flex flex-row w-3/4 md:w-3/4 lg:w-3/4">
-        {/* Close Button at the left side of the modal */}
-        
-        {/* Video Player */}
-        <div className="flex-1">
-          <video src={videoUrl} controls className="w-full max-h-96" autoPlay muted />
-        </div>
-        {/* How to do / Description */}
-        <div className="flex-1 p-5">
-          <h2 className="text-lg font-semibold mb-4">{videoName}</h2>
-          <p>{videoHowtodo}</p>
-        </div>
-        <div className="absolute top-0 left-0 m-4">
-          <button onClick={handleClose} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-            Close
-          </button>
-        </div>
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 overflow-auto">
+    <div className="relative bg-white p-6 rounded-lg shadow-lg flex flex-col md:flex-row w-full max-w-3xl m-4">
+      {/* Other Modal Content */}
+      <div className="mb-4 md:mb-0 md:flex-1">
+        <video src={videoUrl} controls className="w-full max-h-96" autoPlay muted />
+      </div>
+      <div className="md:flex-1 p-5">
+        <h2 className="text-lg font-semibold mb-4">{videoName}</h2>
+        <p>{videoHowtodo}</p>
+      </div>
+      {/* Close Button at the bottom left of the modal */}
+      <div className="absolute bottom-0 left-0 m-4">
+        <button onClick={handleClose} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          Close
+        </button>
       </div>
     </div>
+  </div>
   );
 };
 
